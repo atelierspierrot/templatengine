98f4b741a1a7719733f24fb07e97d8bd89114641
Piero Wbmstr <me@e-piwi.fr>
2014-06-14 10:02:54 +0200

    new piwi github username

--- src/Assets/Composer/TemplateEngineConfig.php
+++ src/Assets/Composer/TemplateEngineConfig.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Template Engine - PHP framework package
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
- * Sources <https://github.com/atelierspierrot/templatengine>
+ * Sources <http://github.com/atelierspierrot/templatengine>
  */
 
 namespace Assets\Composer;
@@ -12,7 +12,7 @@ namespace Assets\Composer;
 use AssetsManager\Config\DefaultConfig;
 
 /**
- * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @author 		Piero Wbmstr <me@e-piwi.fr>
  */
 class TemplateEngineConfig
     extends DefaultConfig
--- src/Assets/Package/Package.php
+++ src/Assets/Package/Package.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Template Engine - PHP framework package
- * Copyleft (c) 2013 Pierre Cassat and contributors
+ * Copyleft (c) 2013-2014 Pierre Cassat and contributors
  * <www.ateliers-pierrot.fr> - <contact@ateliers-pierrot.fr>
  * License GPL-3.0 <http://www.opensource.org/licenses/gpl-3.0.html>
- * Sources <https://github.com/atelierspierrot/templatengine>
+ * Sources <http://github.com/atelierspierrot/templatengine>
  */
 
 namespace Assets\Package;
@@ -19,7 +19,7 @@ use AssetsManager\Loader as AssetsLoader,
     AssetsManager\Package\Preset;
 
 /**
- * @author 		Piero Wbmstr <piero.wbmstr@gmail.com>
+ * @author 		Piero Wbmstr <me@e-piwi.fr>
  */
 class Package extends AssetsPackage
 {
